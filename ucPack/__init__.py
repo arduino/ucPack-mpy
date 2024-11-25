90ea5367929b6acb42b9b76aa6f1770c3e65c5ee
Lucio Rossi <lucio.rossi75@gmail.com>
2024-11-25 12:08:26 +0100

    fix: ucPack ver in __init__ is not aligned with package.json

--- ucPack/__init__.py
+++ ucPack/__init__.py
@@ -6,7 +6,7 @@ Micropython porting of ucPack library [https://github.com/gbr1/ucPack]
 
 __author__ = "Lucio Rossi <l.rossi@arduino.cc>, Giovanni Bruno <g.bruno@arduino.cc>"
 __license__ = "MPL 2.0"
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 __maintainer__ = "Lucio Rossi <l.rossi@arduino.cc>, Giovanni Bruno <g.bruno@arduino.cc>"
 
 from .ucPack import *
